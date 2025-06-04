7d6ba6c0e022910892e9b500db9f57dc3bb8b286
groneft <gurahmetemin@gmail.com>
2025-06-04 16:58:39 +0300

    add programcolor.cs

---
+++ programcolor.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DummyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is just for GitHub language detection :)");
+        }
+    }
+}
